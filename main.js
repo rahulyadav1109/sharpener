ee23b89963a24d85d73da5b290d87d5e6d1971d6
rahulyadav1109 <rydddsss1109@gmail.com>
2023-03-21 16:28:56 +0530

    adding index.html

---
+++ main.js
@@ -0,0 +1,9 @@
+
+//console.dir(document)
+
+// console.log(document.domain);
+// console.log(document.URL)
+// console.log(document.doctype);
+
+// console.log(document.links);
+document.getElementById('main-header').style.borderBottom='solid 3px #000'
