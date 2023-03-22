6aeec0ca8d4d6d4466a9cf49113aa4bf5a884980
rahulyadav1109 <rydddsss1109@gmail.com>
2023-03-22 15:35:45 +0530

    task6 javascript changes

--- main.js
+++ main.js
@@ -1,8 +1,26 @@
-var test=document.getElementsByClassName("list-group-item");
+// var test=document.getElementsByTagName("li");
 
-console.log(test[1]);
-test[1].style.backgroundColor='green'
-for(let i=0;i<test.length;i++)
+// // console.log(test[1]);
+// // test[1].style.backgroundColor='green'
+// // for(let i=0;i<test.length;i++)
+// // {
+// //     test[i].style.fontWeight='bold';
+// // }
+
+// console.log(test[4].style.fontWeight='bold')
+
+let variable=document.querySelector('li:nth-child(1)');
+variable.style.backgroundColor='green'
+
+let visible=document.getElementsByTagName('li')[2];
+visible.style.display='none';
+
+let change=document.querySelectorAll('li:nth-child(2)')[0];
+change.style.color='green';
+
+let odd=document.querySelectorAll('li:nth-child(odd)');
+
+for(let i=0;i<odd.length;i++)
 {
-    test[i].style.fontWeight='bold';
+    odd[i].style.backgroundColor='green';
 }
