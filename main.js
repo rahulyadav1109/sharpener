6cef11fc7650962b946da02873d3f6ae6e378723
rahulyadav1109 <rydddsss1109@gmail.com>
2023-03-22 17:33:10 +0530

    task7

--- main.js
+++ main.js
@@ -9,18 +9,38 @@
 
 // console.log(test[4].style.fontWeight='bold')
 
-let variable=document.querySelector('li:nth-child(1)');
-variable.style.backgroundColor='green'
+// let variable=document.querySelector('li:nth-child(1)');
+// variable.style.backgroundColor='green'
 
-let visible=document.getElementsByTagName('li')[2];
-visible.style.display='none';
+// let visible=document.getElementsByTagName('li')[2];
+// visible.style.display='none';
 
-let change=document.querySelectorAll('li:nth-child(2)')[0];
-change.style.color='green';
+// let change=document.querySelectorAll('li:nth-child(2)')[0];
+// change.style.color='green';
 
-let odd=document.querySelectorAll('li:nth-child(odd)');
+// let odd=document.querySelectorAll('li:nth-child(odd)');
 
-for(let i=0;i<odd.length;i++)
-{
-    odd[i].style.backgroundColor='green';
-}
+// for(let i=0;i<odd.length;i++)
+// {
+//     odd[i].style.backgroundColor='green';
+// }
+
+var newdiv=document.createElement('div');
+newdiv.className='hello';
+newdiv.id='hello1';
+
+newdiv.setAttribute('title','Hello Div');
+
+var newdivtext=document.createTextNode('Hello');
+
+newdiv.appendChild(newdivtext);
+
+var container=document.querySelector('header .container');
+var h1=document.querySelector('header h1');
+
+console.log(newdiv)
+container.insertBefore(newdiv,h1);
+
+var parentnode= document.getElementById('items');
+console.log(parentnode.innerHTML);
+parentnode.innerHTML='<li>Hello World</li>' + parentnode.innerHTML;
